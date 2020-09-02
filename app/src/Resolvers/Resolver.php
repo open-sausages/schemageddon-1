9137f2e74808c4fecd5f2aab2a6f9d221fd0c098
Aaron Carlino <unclecheese@leftandmain.com>
2020-09-02 14:11:28 +1200

    Fix paginate plugin

--- app/src/Resolvers/Resolver.php
+++ app/src/Resolvers/Resolver.php
@@ -3,7 +3,11 @@
 namespace MyProject\Resolvers;
 
 use Faker\Factory;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\GraphQL\Schema\Plugin\PaginationPlugin;
 use SilverStripe\GraphQL\Schema\Resolver\DefaultResolverProvider;
+use SilverStripe\i18n\Data\Locales;
+use Closure;
 
 class Resolver extends DefaultResolverProvider
 {
@@ -32,4 +36,36 @@ class Resolver extends DefaultResolverProvider
 
         return $faker->address;
     }
+
+    public static function resolveReadCountries($obj, array $args = [])
+    {
+        $results = [];
+        $countries = Injector::inst()->get(Locales::class)->getCountries();
+        foreach ($countries as $code => $name) {
+            $results[] = [
+                'code' => $code,
+                'name' => $name
+            ];
+        }
+
+        return $results;
+    }
+
+    public static function paginateCountries(array $context): Closure
+    {
+        $maxLimit = $context['maxLimit'];
+        return function (array $countries, array $args) use ($maxLimit) {
+            $offset = $args['offset'];
+            $limit = $args['limit'];
+            $total = count($countries);
+            if ($limit > $maxLimit) {
+                $limit = $maxLimit;
+            }
+
+            $limitedList = array_slice($countries, $offset, $limit);
+
+            return PaginationPlugin::createPaginationResult($total, $limitedList, $limit, $offset);
+        };
+    }
+
 }
